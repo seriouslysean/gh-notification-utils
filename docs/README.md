9d99742f14933e21917693b84d9aeb2979d278b8
Sean Kennedy <skennedy@urbn.com>
2020-08-19 10:52:15 -0400

    Initial commit

---
+++ docs/README.md
@@ -0,0 +1,10 @@
+# GH Notification Utils
+
+## Add to Chrome
+
+* Download or clone this repo
+* Set up Chrome Extensions for development, https://developer.chrome.com/extensions/faq#faq-dev-01
+* Click `Load Unpacked` and select the `gh-notification-utils` folder
+* Go to your GitHub Notifications, https://github.com/notifications
+
+![GitHub Notification Utils](gh-notification-utils.png)
---
+++ utils.js
@@ -0,0 +1,61 @@
+function unwatchMergedAndClosedPrs(e) {
+    e.preventDefault();
+    document.querySelectorAll('.js-notifications-list-item').forEach((item) => {
+        const isMerged = item.querySelector('.octicon-git-merge') !== null;
+        const isClosed = item.querySelector('.octicon-git-pull-request.text-red') !== null;
+        if (isMerged || isClosed) {
+            const unsubscribeForm = item.querySelector('button[title="Unsubscribe"]');
+            if (unsubscribeForm !== null) {
+                unsubscribeForm.click();
+            }
+        }
+    });
+}
+
+function createContainer(title) {
+    const container = document.createElement('div');
+    container.classList.add('cs-github-notifications');
+    const hr = document.createElement('hr');
+    container.appendChild(hr);
+    const p = document.createElement('p');
+    p.className = 'h6 border-0 text-gray py-2 pl-2 m-0';
+    const node = document.createTextNode(title);
+    p.appendChild(node);
+    container.appendChild(p);
+    return container;
+}
+
+function createList() {
+    const list = document.createElement('ul');
+    list.className = 'filter-list pt-2 pl-2';
+    return list;
+}
+
+function createLink(text) {
+    const link = document.createElement('a');
+    link.className = 'btn-link link-gray';
+    const node = document.createTextNode(text);
+    link.appendChild(node);
+    return link;
+}
+
+function init() {
+    const target = document.querySelector('.notification-navigation');
+    if (target === null) {
+        // Unable to find notification sidebar
+        return;
+    }
+
+    const container = createContainer('Utils');
+    const list = createList();
+    const unwatchLink = createLink('Unwatch Closed/Merged PRs');
+    unwatchLink.addEventListener('click', unwatchMergedAndClosedPrs);
+    list.appendChild(unwatchLink);
+    container.appendChild(list);
+    container.setAttribute('style', 'opacity: 0.5;');
+    target.appendChild(container);
+}
+
+window.addEventListener('load', (event) => {
+    init();
+});
